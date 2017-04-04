a1a699ba968da1dd8c20f08c118f3faaa02fe720
Ihor Kolodyuk <ik@jelastic.com>
2017-04-04 16:59:50 +0300

    upd

--- jelastic/scripts/deploy.sh
+++ jelastic/scripts/deploy.sh
@@ -49,14 +49,12 @@ e -q; }
 
     unzip  -Z1 ${DOWNLOADS}/${package_name} |  grep -q "META-INF/MANIFEST.MF" && {
     BASEDIR=""
-    cp  ${DOWNLOADS}/${package_name} ${WEBROOT}/app.jar && writeJSONResponseOut "result=>0" "message=>Application deployed succesfully";
+    cp  ${DOWNLOADS}/${package_name} ${WEBROOT}/${package_name} && writeJSONResponseOut "result=>0" "message=>Application deployed succesfully";
          } ||  local jar_entry=$(unzip  -Z1 ${DOWNLOADS}/${package_name}   | grep ".jar" | head -1 );
         [ ! -z $jar_entry ]  && {
          unzip -o "$DOWNLOADS/$package_name" -d "${WEBROOT}" 2>>$ACTIONS_LOG 1>/dev/null;
-        BASEDIR=$(dirname "${WEBROOT}/${jar_entry}");
-        ln -sf "${WEBROOT}/${jar_entry}" ${BASEDIR}/app.jar  && writeJSONResponseOut "result=>0" "message=>Application deployed succesfully";
-          cp  ${DOWNLOADS}/${package_name} ${WEBROOT}/app.jar && writeJSONResponseOut "result=>0" "message=>Application deployed succesfully";
-                            } || writeJSONResponseErr "result=>4060" "message=>Application deployed with error";
+        BASEDIR=$(dirname "${WEBROOT}/${jar_entry}"); 
+                                } || writeJSONResponseErr "result=>4060" "message=>Application deployed with error";
     sed -i "s@export BASEDIR=.*@export BASEDIR=$BASEDIR@" $crt_control;
     clearCache;
     startService ${SERVICE} > /dev/null 2>&1;

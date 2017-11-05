bb98129f6786faeb27cdaf433f7741ea928403cb
Dmytro Zubelevych <dz@jelastic.com>
2017-11-05 19:08:09 +0200

    added generate-optimal-configs script

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -1,4 +1,12 @@
-#!/bin/bash        
+#!/bin/bash
+
+echo -e "$(find $(realpath /usr/java/latest) -name libjli.so -printf "%h\n")" > /etc/ld.so.conf.d/java.conf ; \
+ldconfig
+
+JAVABIN=$(which java 2>/dev/null)
+[ ! -z "$JAVABIN" ] && {
+    setcap 'cap_net_bind_service=+ep' $(readlink -f "$JAVABIN")
+}        
 
 if ! `echo $JAVA_OPTS | grep -q "\-Xms[[:digit:]\.]"`
 then
